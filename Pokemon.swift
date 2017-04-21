eae5b894fa2d53e2f1f7da6ca91d34dd5cc0afe1
larenSakota <jedi.temple@yahoo.com>
2017-04-21 14:10:04 -0700

    created pokemon class

---
+++ Pokemon.swift
@@ -0,0 +1,32 @@
+//
+//  Pokemon.swift
+//  pokedex training
+//
+//  Created by Laren Sakota on 4/21/17.
+//  Copyright © 2017 3..2..1..GO. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        
+        return _name
+    }
+    
+    var pokedexID: Int {
+        
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
