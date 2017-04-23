7e3e2c5bbfaf39256e15686b1cabd320a7f2b7c5
larenSakota <jedi.temple@yahoo.com>
2017-04-22 19:58:51 -0700

    create custom view cell

---
+++ PokeCell.swift
@@ -0,0 +1,27 @@
+//
+//  PokeCell.swift
+//  pokedex training
+//
+//  Created by Laren Sakota on 4/22/17.
+//  Copyright © 2017 3..2..1..GO. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexID)")
+        
+    }
+    
+}
